c029c35324077a37235539eedcd253e933f12cb5
Jitendra Singh <jitendra.singh2@moodys.com>
2019-10-14 15:32:30 +0530

    Added files for initial setup

---
+++ test_svc_controller.py
@@ -0,0 +1,20 @@
+import unittest
+from controller.svc_controller import app, wrap_html
+
+class TestSVCController(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_status_code(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_message(self):
+        response = self.app.get('/')
+        message = wrap_html('Hello DockerCon 2019!')
+        self.assertEqual(response.data.decode("utf-8"), message)
+
+if __name__ == '__main__':
+    unittest.main()
